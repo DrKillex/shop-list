8b53868f2d17a51134f7be71e9568c2c164302ce
DrKillex <lcollaro1994@gmail.com>
2023-08-26 17:24:07 +0200

    aggiunto titolo e commenti

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// utility
 const nome = document.getElementById('nome');
 const oggetto = document.getElementById('oggetto');
 const prezzo = document.getElementById('prezzo');
@@ -12,23 +13,17 @@ let totalPrice = 0;
 
 let list = [];
 
-if(controlList){
-    list = JSON.parse(controlList)
-    displayList();
-    deleteEvent();
-} else {
-    priceDiv.innerHTML=''
-    listDiv.innerHTML=`<div>nessun elemento in lista</div>`;
-}
+// funzioni
 
-function displayList(){
-    if (list.length>0){
-        totalPrice=0
-        listDiv.innerHTML='';
+// funzione per visualizzazione lista
+function displayList() {
+    if (list.length > 0) {
+        totalPrice = 0
+        listDiv.innerHTML = '';
         for (let i = 0; i < list.length; i++) {
-            totalPrice=Number(totalPrice)+Number(list[i].price)
-            listDiv.innerHTML+=
-            `<div class="card">
+            totalPrice = Number(totalPrice) + Number(list[i].price)
+            listDiv.innerHTML +=
+                `<div class="card">
                 <div class="card-body">
                     <div class='row'>
                         <div class='col-12 d-flex'>
@@ -43,33 +38,45 @@ function displayList(){
                 </div>
             </div>`
         };
-        priceDiv.innerHTML=`${totalPrice}$`
-        deleteListElements = document.querySelectorAll('.delete');   
+        priceDiv.innerHTML = `${totalPrice}$`
+        deleteListElements = document.querySelectorAll('.delete');
     } else {
-        priceDiv.innerHTML=''
-        listDiv.innerHTML=`<div>nessun elemento in lista</div>`;
+        priceDiv.innerHTML = ''
+        listDiv.innerHTML = `<div>nessun elemento in lista</div>`;
     }
 };
 
-function deleteEvent(){
-    for (let i = 0; i < deleteListElements.length; i++){
+// funzione per aggiungere evento delete a elemento lista
+function deleteEvent() {
+    for (let i = 0; i < deleteListElements.length; i++) {
         const deleteListElement = deleteListElements[i];
-        deleteListElement.addEventListener('click', function(){
+        deleteListElement.addEventListener('click', function () {
             list.splice(deleteListElement.dataset.position, 1);
             displayList();
             deleteEvent();
             console.log(list)
-            if(list.length>0){
+            if (list.length > 0) {
                 localStorage.setItem('list', JSON.stringify(list))
             } else {
                 localStorage.setItem('list', [])
             }
-            
+
         })
     };
 };
 
-submit.addEventListener('click', function(){
+// controllo se presente una lista in local storage
+if (controlList) {
+    list = JSON.parse(controlList)
+    displayList();
+    deleteEvent();
+} else {
+    priceDiv.innerHTML = ''
+    listDiv.innerHTML = `<div>nessun elemento in lista</div>`;
+}
+
+// evento aggiunta nuovo elemento in lista
+submit.addEventListener('click', function () {
     list.push(
         {
             name: nome.value,
@@ -77,9 +84,9 @@ submit.addEventListener('click', function(){
             price: prezzo.value
         }
     )
-    nome.value=null;
-    oggetto.value=null;
-    prezzo.value=null;
+    nome.value = null;
+    oggetto.value = null;
+    prezzo.value = null;
     displayList();
     deleteEvent();
     console.log(list)
